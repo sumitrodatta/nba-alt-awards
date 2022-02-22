9f11935128f5ca96128377986bb994d4bb0b6357
Sumitro Datta <sumitrodatta@gmail.com>
2022-02-21 20:43:35 -0500

    all-star break update

--- 2022/r-specific awards.R
+++ 2022/r-specific awards.R
@@ -24,21 +24,16 @@ play_by_play=read_csv("Data/Player Play by Play.csv") %>% filter(season==2022)
 #The Real Sixth Man of the Year (presented by Brent Barry)*
 
 #sixth man winners if sixth to ninth on team in minutes played
-smoy_winners=advanced %>% left_join(.,per_game) %>%
-  left_join(.,team_summaries) %>% mutate(g_percent=g/g_total) %>% filter(g_percent>=0.5) %>% 
-  mutate(mp_per_game=mp/g,.after="mp") %>% arrange(desc(mp_per_game)) %>% 
+smoy_candidates=advanced %>% left_join(.,per_game) %>%
+  left_join(.,team_summaries) %>% mutate(g_percent=g/g_total) %>% filter(g_percent>=0.5,tm != "TOT") %>% 
+  filter(!is.na(mp_per_game)) %>% arrange(desc(mp_per_game)) %>% 
   group_by(tm,season) %>% slice(6:9) %>% ungroup()
 
-smoy_winners %>% slice_max(vorp,n=5) %>% 
+smoy_candidates %>% slice_max(vorp,n=5) %>% 
   select(player,pos:experience,tm,g,gs,mp_per_game,vorp)
 
-smoy_winners_ppg=per_game %>%
-  left_join(.,team_summaries) %>% mutate(g_percent=g/g_total) %>% filter(g_percent>=0.5) %>%
-  filter(!is.na(mp_per_game)) %>% arrange(desc(mp_per_game)) %>% group_by(tm,season) %>% 
-  slice(6:9) %>% ungroup()
-
-smoy_winners_ppg %>% slice_max(pts_per_game,n=5) %>% 
-  select(player,pos:experience,tm,g,mp_per_game,pts_per_game)
+smoy_candidates %>% slice_max(pts_per_game,n=5) %>% 
+  select(player,pos:experience,tm,g,gs,mp_per_game,pts_per_game)
 
 # The Deadshot Award (presented by Ray Allen/Reggie Miller)
 
@@ -69,19 +64,20 @@ per_game %>% left_join(.,team_summaries) %>% mutate(g_percent=g/g_total,total_mp
 # The Weakest Link award (sponsored by Jack Link's Beef Jerky, presented by the 2015 Atlanta Hawks Starting 5)*
 
 #best 5th starter (must have started 50% of a team's games) (credit to memeticengineering for the idea)
-best_worst_starter_winners=advanced %>% left_join(.,per_game) %>%
+best_worst_starter_candidates=advanced %>% left_join(.,per_game) %>%
   left_join(.,team_summaries) %>% mutate(start_percent=gs/g_total) %>% 
   filter(start_percent>=0.5) %>% mutate(mp_per_game=mp/g,.after="mp") %>% 
   arrange(desc(mp_per_game)) %>% group_by(tm,season) %>% slice(1:5) %>% slice_min(vorp) %>% ungroup() 
 
-best_worst_starter_winners %>% slice_max(vorp,n=5) %>% select(season,player,pos:experience,tm,g_total,gs,mp_per_game,vorp)
+best_worst_starter_candidates %>% slice_max(vorp,n=5) %>% select(player,pos:experience,tm,g_total,gs,mp_per_game,vorp)
 
 # The Empty Calorie Stats Award (sponsored by Pop-Tarts)
 
 # highest percentile rank within position in usage, descending VORP, descending TS% (credit to eewap for the idea)
 
 empty_stats_df=advanced %>% 
-  left_join(.,team_summaries) %>% mutate(g_percent=g/g_total) %>% filter(g_percent>=0.5) %>% group_by(pos) %>%
+  left_join(.,team_summaries) %>% mutate(g_percent=g/g_total) %>% filter(g_percent>=0.5) %>%
+  mutate(pos=word(pos,sep="-")) %>% group_by(pos) %>%
     mutate(ts_rank=percent_rank(desc(ts_percent)),
            usg_rank=percent_rank(usg_percent),
            vorp_rank=percent_rank(desc(vorp)),
@@ -115,9 +111,10 @@ driver<- rsDriver(browser=c("firefox"))
 remDr <- driver[["client"]]
 remDr$open()
 remDr$navigate("https://www.pbpstats.com/totals/nba/player?Season=2021-22&SeasonType=Regular%20Season&Type=Player&StatType=Totals&Table=FTs")
+Sys.sleep(10)
 a<-remDr$findElement(using="xpath",value='//*[contains(concat( " ", @class, " " ), concat( " ", "footer__row-count__select", " " ))]')
 a$clickElement()
-Sys.sleep(3)
+Sys.sleep(10)
 a$clickElement()
 b<-remDr$findElement(using="xpath",value="//*/option[@value = '500']")
 b$clickElement()
