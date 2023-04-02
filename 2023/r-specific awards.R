5628761b2feeebceb60866bdd0c38df3f55ca7e0
Sumitro Datta <sumitrodatta@gmail.com>
2023-04-02 17:27:29 -0400

    awards as of apr 1

--- 2023/r-specific awards.R
+++ 2023/r-specific awards.R
@@ -24,8 +24,6 @@ totals=read_csv("Data/Player Totals.csv") %>% filter(season==2023) %>%
   mutate(tm=ifelse(tm=="1TOT","TOT",tm)) %>% 
   arrange(season,player) %>% ungroup()
 
-num_players_season=nrow(advanced)
-
 team_summaries=read_csv("Data/Team Summaries.csv") %>% filter(season==2023) %>% mutate(g_total=w+l) %>% 
   rename(tm=abbreviation) %>% select(season,tm,g_total) %>% head(.,-1) %>% add_row(season=2023,tm="TOT",g_total=mean(.$g_total))
 teams=advanced %>% filter(season==2023,tm != "TOT") %>% distinct(tm) %>% arrange(tm) %>% pull(tm)
@@ -67,6 +65,18 @@ per_game %>% left_join(.,team_summaries) %>% filter(x3p_per_game*g>82,x3p_per_ga
 per_game %>% left_join(.,team_summaries) %>% filter(fg_per_game*g>=300,fg_per_game>=300/82) %>%
   slice_min(x2p_percent,n=5) %>% select(player,x2p_per_game,x2p_percent)
 
+# The "If He Dies, He Dies" Award (presented by Tom Thibodeau, sponsored by Ivan Drago)
+
+#most minutes played per game (Basketball-Reference) (credit to FurryCrew for the idea)
+
+per_game %>% left_join(.,team_summaries) %>% mutate(g_percent=g/g_total) %>% filter(g_percent>=0.7) %>%
+  slice_max(mp_per_game,n=5) %>% select(player,mp_per_game)
+
+#alternatively: most total minutes played (Basketball-Reference) (credit to FrankEMartindale for the idea)
+
+per_game %>% left_join(.,team_summaries) %>% mutate(g_percent=g/g_total,total_mp=g*mp_per_game) %>% 
+  filter(g_percent>=0.7) %>% slice_max(total_mp,n=5) %>% select(player,total_mp)
+
 # The Black Hole Award
 
 #most FGAs per assist (credit to Moose4KU for the idea)
@@ -96,27 +106,41 @@ totals %>%
   mutate(fouls_per_36_minutes=pf/mp*36) %>% select(player,season,mp,pf,fouls_per_36_minutes) %>%
   slice_max(fouls_per_36_minutes,n=5)
 
-# The "If He Dies, He Dies" Award (presented by Tom Thibodeau, sponsored by Ivan Drago)
-
-#most minutes played per game (Basketball-Reference) (credit to FurryCrew for the idea)
-
-per_game %>% left_join(.,team_summaries) %>% mutate(g_percent=g/g_total) %>% filter(g_percent>=0.7) %>%
-  slice_max(mp_per_game,n=5) %>% select(player,mp_per_game)
-
-#alternatively: most total minutes played (Basketball-Reference) (credit to FrankEMartindale for the idea)
-
-per_game %>% left_join(.,team_summaries) %>% mutate(g_percent=g/g_total,total_mp=g*mp_per_game) %>% 
-  filter(g_percent>=0.7) %>% slice_max(total_mp,n=5) %>% select(player,total_mp)
-
 # The Weakest Link award (sponsored by Jack Link's Beef Jerky, presented by the 2015 Atlanta Hawks Starting 5)*
 
 #best 5th starter (must have started 50% of a team's games) (credit to memeticengineering for the idea)
 best_worst_starter_candidates=advanced %>% left_join(.,per_game) %>%
   left_join(.,team_summaries) %>% mutate(start_percent=gs/g_total) %>% 
-  filter(start_percent>=0.5) %>% mutate(mp_per_game=mp/g,.after="mp") %>% 
+  filter(start_percent>=0.5,tm !="TOT") %>% mutate(mp_per_game=mp/g,.after="mp") %>% 
   arrange(desc(mp_per_game)) %>% group_by(tm,season) %>% slice(1:5) %>% slice_min(vorp) %>% ungroup() 
 
 best_worst_starter_candidates %>% slice_max(vorp,n=5) %>% select(player,pos:experience,tm,g_total,gs,mp_per_game,vorp)
+
+# The "Master Baiter" Award (sponsored by Bass Pro Shops & Kleenex)
+
+# most 3-point shooting fouls drawn (PBPStats.com)
+
+driver<- rsDriver(browser=c("firefox"),port=4444L)
+remDr <- driver[["client"]]
+remDr$open()
+remDr$navigate("https://www.pbpstats.com/totals/nba/player?Season=2022-23&SeasonType=Regular%20Season&Type=Player&StatType=Totals&Table=FTs")
+Sys.sleep(20)
+a<-remDr$findElement(using="xpath",value='//*[contains(concat( " ", @class, " " ), concat( " ", "footer__row-count__select", " " ))]')
+a$clickElement()
+Sys.sleep(10)
+a$clickElement()
+b<-remDr$findElement(using="xpath",value=paste0("//*/option[@value = '500']"))
+b$clickElement()
+
+ft_source=read_html(remDr$getPageSource()[[1]]) %>% html_nodes("table") %>% .[[1]] %>% html_table() %>% 
+  select(-1) %>% rename_with(.fn=~word(.,1,sep="\n")) %>% slice(-1) %>% clean_names() %>% 
+  mutate(three_pt_shooting_fouls_drawn=x3pt_sfd+x3pt_and_1s)
+
+remDr$close()
+driver$server$stop()
+
+ft_source %>% slice_max(three_pt_shooting_fouls_drawn,n=5) %>% 
+  select(name,x3pt_sfd,x3pt_and_1s,three_pt_shooting_fouls_drawn)
 
 # The "This Game Has Always Been, And Will Always Be, About Buckets" Award (https://www.youtube.com/watch?v=-xYejfYxT4s)
 
@@ -187,6 +211,33 @@ on_off_diff_trail %>% slice_min(npm_diff,n=5) %>% select(player:npm_diff)
 write_csv(full_join(on_off_diff,on_off_diff_trail) %>% 
             select(season,player:avg_wo_trailer),"Output Data/On-Off Difference With & Without Extreme Player.csv")
 
+# The Stonks Award
+
+#contract overperformance by fewest contract $ per 1 VORP (credit to memeticengineering for the idea)
+contracts=tibble()
+for (x in teams) {
+  session=nod(bbref_bow,path=paste0("contracts/",x,".html"))
+  teams_contracts=scrape(session) %>% html_nodes(xpath='//*[(@id = "contracts")]') %>% 
+    html_table() %>% .[[1]] %>% row_to_names(row_number=1) %>% clean_names() %>% 
+    filter(age != "",x2022_23 != "") %>% select(player,x2022_23) %>% rename(salary=x2022_23) %>% 
+    mutate(salary=parse_number(salary),tm=x)
+  contracts=bind_rows(contracts,teams_contracts)
+  print(x)
+}
+
+salary_performance=left_join(contracts,read_csv("Data/Advanced.csv") %>% filter(season==2023)) %>% 
+  select(player,tm,experience,age,salary,vorp) %>% 
+  mutate(vorp_per_million=vorp/salary*1000000) %>% 
+  mutate(percent_of_cap=salary/123655000) %>% filter(!is.na(vorp))
+#remove any player with <=4 years of experience (rookie contract), players w/lower salary than min (10-days)
+salary_performance %>% filter(experience > 4,salary>1017781) %>% arrange(desc(vorp_per_million))
+#remove players whose salary is less than 5% of cap
+salary_performance %>% filter(experience > 4,percent_of_cap>0.05) %>% arrange(desc(vorp_per_million))
+
+write_csv(salary_performance,"Output Data/Salary Overperformance.csv")
+
+# rotation awards
+
 player_heights=tibble()
 for (x in teams) {
   session=nod(bbref_bow,path=paste0("teams/",x,"/2023.html"))
@@ -221,55 +272,6 @@ pos_percents_w_heights=play_by_play %>% select(seas_id:player,pos,tm:c_percent) 
 pos_percents_w_heights %>% filter(mp>50) %>% ggplot(aes(x=full_in_ht,fill=position)) + 
   geom_bar() + dark_theme_grey()
 
-write_csv(pos_percents_w_heights,"Output Data/Player Positions.csv")
-
-# The "Master Baiter" Award (sponsored by Bass Pro Shops & Kleenex)
-
-# most 3-point shooting fouls drawn (PBPStats.com)
-
-driver<- rsDriver(browser=c("firefox"),port=4444L)
-remDr <- driver[["client"]]
-remDr$open()
-remDr$navigate("https://www.pbpstats.com/totals/nba/player?Season=2022-23&SeasonType=Regular%20Season&Type=Player&StatType=Totals&Table=FTs")
-Sys.sleep(20)
-a<-remDr$findElement(using="xpath",value='//*[contains(concat( " ", @class, " " ), concat( " ", "footer__row-count__select", " " ))]')
-a$clickElement()
-Sys.sleep(10)
-a$clickElement()
-b<-remDr$findElement(using="xpath",value=paste0("//*/option[@value = '",num_players_season,"']")) #number of players in the season
-b$clickElement()
-
-ft_source=read_html(remDr$getPageSource()[[1]]) %>% html_nodes("table") %>% .[[1]] %>% html_table() %>% 
-  select(-1) %>% rename_with(.fn=~word(.,1,sep="\n")) %>% slice(-1) %>% clean_names() %>% 
-  mutate(three_pt_shooting_fouls_drawn=x3pt_sfd+x3pt_and_1s)
-
-remDr$close()
-driver$server$stop()
-
-ft_source %>% slice_max(three_pt_shooting_fouls_drawn,n=5) %>% 
-  select(name,x3pt_sfd,x3pt_and_1s,three_pt_shooting_fouls_drawn)
-
-# The Stonks Award
-
-#contract overperformance by fewest contract $ per 1 VORP (credit to memeticengineering for the idea)
-contracts=tibble()
-for (x in teams) {
-  session=nod(bbref_bow,path=paste0("contracts/",x,".html"))
-  teams_contracts=scrape(session) %>% html_nodes(xpath='//*[(@id = "contracts")]') %>% 
-    html_table() %>% .[[1]] %>% row_to_names(row_number=1) %>% clean_names() %>% 
-    filter(age != "",x2022_23 != "") %>% select(player,x2022_23) %>% rename(salary=x2022_23) %>% 
-    mutate(salary=parse_number(salary),tm=x)
-  contracts=bind_rows(contracts,teams_contracts)
-  print(x)
-}
-
-salary_performance=left_join(contracts,read_csv("Data/Advanced.csv") %>% filter(season==2023)) %>% 
-  select(player,tm,experience,age,salary,vorp) %>% 
-  mutate(vorp_per_million=vorp/salary*1000000) %>% 
-  mutate(percent_of_cap=salary/123655000) %>% filter(!is.na(vorp))
-#remove any player with <=4 years of experience (rookie contract), players w/lower salary than min (10-days)
-salary_performance %>% filter(experience > 4,salary>1017781) %>% arrange(desc(vorp_per_million))
-#remove players whose salary is less than 5% of cap
-salary_performance %>% filter(experience > 4,percent_of_cap>0.05) %>% arrange(desc(vorp_per_million))
-
-write_csv(salary_performance,"Output Data/Salary Overperformance.csv")
+write_csv(pos_percents_w_heights %>% filter(mp/g>=12) %>% group_by(tm,position_2) %>% 
+            group_by(tm,position_2) %>% mutate(leader=if_else(max(vorp)==vorp,TRUE,FALSE)) %>% ungroup(),
+          "Output Data/Player Positions.csv")
